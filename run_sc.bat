afbaa56be7f4b420a40af550a7d07688257e3faf
taigameface <tai@gameface.ai>
2024-11-12 20:54:23 +1100

    Revert "FIX PREVIOUS COMMIT"
    This reverts commit db1f15cc17d70d4078486449c379dd8c746aa8b8.

--- run_sc.bat
+++ run_sc.bat
@@ -1,5 +1,4 @@
 start http://localhost:8080/dashboard.html
 cd node_files
 node update.js
-timeout /t 0.1
 node server.js
--- run_sc.sh
+++ run_sc.sh
@@ -3,5 +3,4 @@ url="http://localhost:8080/dashboard.html"
 open -a "Chrome" "$url"
 cd node_files
 node update.js
-sleep .1
 node server.js
